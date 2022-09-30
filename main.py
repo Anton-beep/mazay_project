9e986dd09e3d61e23b833fd8a690b06e4e189e5a
Anton-Beep <anton.plyu@gmail.com>
2022-09-30 17:01:31 +0200

    data loading

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import csv
-from dataclasses import dataclass, KW_ONLY
+import os
+import re
+from dataclasses import dataclass
 from typing import Optional, List
+
+DATA_FILE = 'data.txt'
+EMAIL_PATTERN = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 
 
 @dataclass
 class Contact:
-    id: int
     surname: str
 
-    _: KW_ONLY
     name: Optional[str]
     father_name: Optional[str]
     email: Optional[str]
@@ -24,6 +27,39 @@ class SingletonClass(object):
 
 class Data(SingletonClass):
     _contacts_list: List[Contact]
+
+    def read_data_from_file(self, file_name: str):
+        if not os.path.exists(file_name):
+            raise NameError(f'Cannot read "{file_name}"')
+
+        _contacts_list = list()
+        with open(file_name, 'r', encoding='utf-8') as data_file:
+            csv_reader = csv.reader(data_file, delimiter=',')
+            for row in csv_reader:
+                surname, name, father_name = row[0].split() + [None for i in range(3 - len(row[0].split()))]
+                # using regex to check email
+                if row[2] == '':
+                    email = None
+                elif re.match(EMAIL_PATTERN, row[2]) is not None:
+                    raise ValueError(f'Invalid email: {row[2]}')
+                else:
+                    email = row[2]
+                # check phone number
+                if row[1] == '':
+                    phone = None
+                elif not row[1][1:].replace('+', '').isnumeric():
+                    raise ValueError(f'Invalid phone number: {row[1]}')
+                else:
+                    phone = row[1]
+                if surname is None:
+                    raise ValueError('Surname has to be not None')
+                _contacts_list.append(Contact(
+                    surname,
+                    name,
+                    father_name,
+                    email,
+                    phone
+                ))
 
     def find_by_number(self, number: str) -> Optional[Contact]:
         results = [element for element in self._contacts_list if element.phone == number]
@@ -42,3 +78,7 @@ class Data(SingletonClass):
         target_line = target_line.rstrip()
 
         return None
+
+
+data = Data()
+data.read_data_from_file(DATA_FILE)
