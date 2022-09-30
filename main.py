e34ec408c670c986df2935162885d6b2eb12c7e4
Anton-Beep <anton.plyu@gmail.com>
2022-09-30 17:57:52 +0200

    bib

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ class Contact:
 
 
 class SingletonClass(object):
+    """Parent class. Contains only one parameter"""
+    instance = None
+
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(SingletonClass, cls).__new__(cls)
